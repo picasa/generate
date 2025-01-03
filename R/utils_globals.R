dd73d3654d6823b5dc65a60b5f6d3a8268f438fc
Pierre Casadebaig <pierre.casa@gmail.com>
2025-01-03 11:16:49 +0100

    text layout system : added the possibility for words to overflow initial line cuts.

--- R/utils_globals.R
+++ R/utils_globals.R
@@ -1,7 +1,7 @@
 utils::globalVariables(c(
   ".", "NOM_DALLE", "V1", "V2", "a", "buffer", "c_l", "c_n", "cl", "d", "data",
-  "dz", "e_c", "e_w", "geometry", "glyph", "group", "i", "id", "l", "layout",
-  "line", "n","p","part", "path", "pattern", "plot_ld", "position", "r", "s",
+  "dz", "e_c", "e_w", "flow", "geometry", "glyph", "group", "i", "id", "l", "layout",
+  "line", "n", "offset", "p","part", "path", "pattern", "plot_ld", "position", "r", "s",
   "shade", "theta", "variation", "word", "x", "x0", "xend", "xn", "xp",
   "x_rank", "y", "y0" ,"y_dist", "y_rank", "yend", "z", "zl", "zl_n", "zn",
   "zs", "z_slope", ":=", "rotate", "shift", "sentences","translate"))
