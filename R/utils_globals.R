09fcbd2f05d2a80e09b0dd92d4beea57009ae6f9
Pierre Casadebaig <pierre.casa@gmail.com>
2025-02-07 18:02:07 +0100

    added an option for vector font text rendering.

--- R/utils_globals.R
+++ R/utils_globals.R
@@ -1,8 +1,9 @@
 utils::globalVariables(c(
-  ".", "NOM_DALLE", "V1", "V2", "a", "buffer", "c_l", "c_n", "cl", "d", "data",
-  "dz", "e_c", "e_w", "flow", "geometry", "glyph", "group", "i", "id", "l", "layout",
-  "line", "n", "offset", "p","part", "path", "pattern", "plot_ld", "position", "r", "s",
-  "shade", "theta", "variation", "word", "x", "x0", "xend", "xn", "xp",
-  "x_rank", "y", "y0" ,"y_dist", "y_rank", "yend", "z", "zl", "zl_n", "zn",
-  "zs", "z_slope", ":=", "rotate", "shift", "sentences","translate"))
+  ".", "NOM_DALLE", "V1", "V2", "a", "buffer", "c_l", "c_n", "cl", "char",
+  "char_idx", "d", "data", "dz", "e_c", "e_w", "flow", "geometry", "glyph",
+  "group", "i", "id", "l", "layout", "line", "n", "offset", "p","part", "path",
+  "pattern", "plot_ld", "position", "r", "s", "stroke","shade", "theta",
+  "variation",  "word", "x", "x0", "xend", "xn", "xp",  "x_rank", "y", "y0" ,
+  "y_dist", "y_rank", "yend", "z", "zl", "zl_n", "zn", "zs", "z_slope",
+  ":=", "rotate", "shift", "sentences","translate"))
 
