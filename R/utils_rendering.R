b6920cc408566c6f7f77bdef12b363a8af0952bc
Pierre Casadebaig <pierre.casa@gmail.com>
2025-09-27 15:25:40 +0200

    corrected default value in ggplot2 4.0 theme functions

--- R/utils_rendering.R
+++ R/utils_rendering.R
@@ -3,26 +3,27 @@
 
 #' Generate a paper texture based on small fibers
 #' @param n texture density
+#' @param scale orientation of texture elements (upper limit of uniform sampling)
 #' @param jitter length of texture elements
 #' @param strength curvature in texture elements
-#' @param size,color linewidth and color of texture elements
+#' @param size,color,alpha aesthetics of texture elements
 #' @param expansion additive scaling for limits
 #' @return a ggplot object
 #' @export
 gen_paper <- function(
-    n = 30e3, jitter = 1/30, strength = 0.5,
-    size = 1/10, color = "black", expansion = -1/50) {
+    n = 30e3, scale = c(1,1), jitter = 1/30, strength = 0.5,
+    size = 1/10, color = "black", alpha = 1/20, expansion = -1/50) {
 
   plot <- dplyr::tibble(
-    x = stats::runif(n),
-    y = stats::runif(n),
+    x = stats::runif(n, 0, scale[1]),
+    y = stats::runif(n, 0, scale[2]),
     xend = jitter(x, amount = jitter),
     yend = jitter(y, amount = jitter)
   ) |>
     ggplot2::ggplot(ggplot2::aes(x, y, xend = xend, yend = yend)) +
     ggforce::geom_diagonal0(
       strength = strength, linewidth = size,
-      color = color, alpha = 1/20) +
+      color = color, alpha = alpha) +
     ggplot2::scale_x_continuous(expand = expansion(add = expansion)) +
     ggplot2::scale_y_continuous(expand = expansion(add = expansion)) +
     ggplot2::theme_void() + ggplot2::theme(legend.position = "none")
@@ -37,13 +38,13 @@ gen_paper <- function(
 #' @param output string to select "digital" or "plotter" theme.
 #' "plotter" theme strips out panel backgroung and borders for a clean svg output.
 #' @param color background color for digital output, default to an oldish white color.
-#' @param border,width border color and width, default to NA.
+#' @param border,width border color and width, default to NULL.
 #' @param margin a vector of four numeric values for margins (top, right, bottom, left) in mm.
 #' @return a ggplot theme object
 #' @export
 theme_paper <- function(
     output = "digital", color = "#FEFAEE",
-    border = NA, width = NA, margin = rep(0,4)) {
+    border = NULL, width = NULL, margin = rep(0,4)) {
 
   switch (
     output,
